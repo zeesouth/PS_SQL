b1b9a6025bc4f8123b01f82a52a9bb12f360de10
Jisu <jisunam0307@gmail.com>
2023-09-06 09:34:10 +0900

    Confirmation Rate (Leetcode, 1934)

--- leetcode/n1934/description.sql
+++ leetcode/n1934/description.sql
@@ -1,5 +1,5 @@
 SELECT A.USER_ID,
-       ROUND(SUM(IF(ACTION = 'confirmed', 1, 0)) / COUNT(*), 2) confirmation_rate
+       ROUND(AVG(IF(ACTION = 'confirmed', 1, 0)), 2) confirmation_rate
 FROM SIGNUPS A
          LEFT JOIN CONFIRMATIONS B
                    ON A.USER_ID = B.USER_ID
