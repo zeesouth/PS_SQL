385b22a87159142c644300f94daa6412b6477f83
Jisu <jisunam0307@gmail.com>
2023-09-25 17:02:57 +0900

    Human Traffic of Stadium (Leetcode, 601) [re]

--- leetcode/n601/description.sql
+++ leetcode/n601/description.sql
@@ -1,8 +1,7 @@
-with q1 as (select *, id - row_number() over() as id_diff
-            from stadium
-            where people > 99)
+WITH temp AS (select *, id - ROW_NUMBER() OVER(ORDER BY id) AS gap
+              FROM Stadium
+              WHERE people >= 100)
 
-select id, visit_date, people
-from q1
-where id_diff in (select id_diff from q1 group by id_diff having count(*) > 2)
-order by visit_date
+SELECT id, visit_date, people
+FROM temp
+WHERE gap IN (SELECT gap FROM temp GROUP BY gap HAVING COUNT(*) >= 3)
