4970ce1aea619d9a131ee7535e0526f273d5864c
Jisu <jisunam0307@gmail.com>
2023-12-17 14:56:14 +0900

    Consecutive Numbers (Leetcode, 180) [69.03%]

--- leetcode/n180/description.sql
+++ leetcode/n180/description.sql
@@ -1,19 +1,7 @@
-#
-Write your MySQL query statement below
-/*
-LEAD(행, 숫자): 선택한 행 기준 숫자 만큼의 이전 행을 가져오는 함수
-LAG(행, 숫자): 선택한 행 기준 숫자 만큼의 다음 행을 가져오는 함수
-*/
-
-with cte as (
-    select num,
-    lead(num,1) over() num1,
-    lead(num,2) over() num2
-    from logs
-
-)
-
-select distinct num ConsecutiveNums
-from cte
-where (num = num1)
-  and (num = num2)
+SELECT B ConsecutiveNums
+FROM (SELECT LEAD(NUM, 1) OVER() A, NUM B,
+             LAG(NUM, 1)  OVER() C
+      FROM LOGS) T1
+WHERE A = B
+  AND B = C
+GROUP BY B
