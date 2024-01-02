ee0ea406bd5017505d46d1ece9774cb6c097e282
Jisu <jisunam0307@gmail.com>
2024-01-02 23:02:44 +0900

    Consecutive Numbers (Leetcode, 180) [85.52%]

--- leetcode/n180/description.sql
+++ leetcode/n180/description.sql
@@ -1,7 +1,8 @@
-SELECT B ConsecutiveNums
-FROM (SELECT LEAD(NUM, 1) OVER() A, NUM B,
-             LAG(NUM, 1)  OVER() C
-      FROM LOGS) T1
-WHERE A = B
-  AND B = C
-GROUP BY B
+SELECT DISTINCT L1.num AS ConsecutiveNums
+FROM Logs L1,
+     Logs L2,
+     Logs L3
+WHERE L1.id = L2.id - 1
+  AND L1.num = L2.num
+  and L2.id = L3.id - 1
+  and L2.num = L3.num
