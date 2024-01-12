9107e65ea50d7545d5795c5d3885226aee4494e5
Jisu <jisunam0307@gmail.com>
2024-01-12 12:18:28 +0900

    Human Traffic of Stadium (Leetcode, 601) [78.30%]

--- leetcode/n601/description.sql
+++ leetcode/n601/description.sql
@@ -1,7 +1,18 @@
-WITH temp AS (select *, id - ROW_NUMBER() OVER(ORDER BY id) AS gap
-              FROM Stadium
-              WHERE people >= 100)
+WITH T1 AS (SELECT B.ID
+            FROM STADIUM A
+                     JOIN STADIUM B
+                     JOIN STADIUM C
+                          ON A.ID + 1 = B.ID AND B.ID = C.ID - 1
+            WHERE A.PEOPLE >= 100
+              AND B.PEOPLE >= 100
+              AND C.PEOPLE >= 100)
 
-SELECT id, visit_date, people
-FROM temp
-WHERE gap IN (SELECT gap FROM temp GROUP BY gap HAVING COUNT(*) >= 3)
+SELECT *
+FROM STADIUM
+WHERE PEOPLE >= 100
+  AND (
+            (ID - 1) IN (SELECT * FROM T1) OR
+            ID IN (SELECT * FROM T1) OR
+            (ID + 1) IN (SELECT * FROM T1)
+    )
+ORDER BY VISIT_DATE
