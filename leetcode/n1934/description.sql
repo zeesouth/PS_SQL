a77a0fb5a074a13bfda265f565e5191f4d1391b8
Jisu <jisunam0307@gmail.com>
2024-01-21 15:31:46 +0900

    Confirmation Rate (Leetcode, 1934) [43.85%]

--- leetcode/n1934/description.sql
+++ leetcode/n1934/description.sql
@@ -1,6 +1,5 @@
-SELECT A.USER_ID,
-       ROUND(AVG(IF(ACTION = 'confirmed', 1, 0)), 2) confirmation_rate
+SELECT A.USER_ID, ROUND(AVG(IF(B.ACTION = 'confirmed', 1, 0)), 2) confirmation_rate
 FROM SIGNUPS A
-         LEFT JOIN CONFIRMATIONS B
-                   ON A.USER_ID = B.USER_ID
+         LEFT OUTER JOIN CONFIRMATIONS B
+                         ON A.USER_ID = B.USER_ID
 GROUP BY A.USER_ID
