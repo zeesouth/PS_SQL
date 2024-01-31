6a43dda971c242c7a16ba89cf5a1f57b403ff45b
Jisu <jisunam0307@gmail.com>
2024-01-31 09:50:53 +0900

    Monthly Transactions I (Leetcode, 1193) [89.72%]

--- leetcode/n1193/description.sql
+++ leetcode/n1193/description.sql
@@ -1,8 +1,8 @@
-SELECT DATE_FORMAT(TRANS_DATE, '%Y-%m') month,
+SELECT DATE_FORMAT(TRANS_DATE, '%Y-%m') MONTH,
        COUNTRY,
        COUNT(*)                               TRANS_COUNT,
-       SUM(IF(STATE = 'APPROVED', 1, 0))      APPROVED_COUNT,
+       SUM(IF(STATE = 'approved', 1, 0))      APPROVED_COUNT,
        SUM(AMOUNT)                            TRANS_TOTAL_AMOUNT,
-       SUM(IF(STATE = 'APPROVED', AMOUNT, 0)) APPROVED_TOTAL_AMOUNT
+       SUM(IF(STATE = 'approved', AMOUNT, 0)) APPROVED_TOTAL_AMOUNT
 FROM TRANSACTIONS
 GROUP BY MONTH, COUNTRY
