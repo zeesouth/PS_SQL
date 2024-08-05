c893258419b56ff13c8e116264df35330869311b
Jisu <jisunam0307@gmail.com>
2024-08-05 13:50:42 +0900

    Investments in 2016 - (585, Leetcode) [431ms, 80.61%]

--- leetcode/n585/description.sql
+++ leetcode/n585/description.sql
@@ -1,10 +1,14 @@
+WITH A AS (SELECT TIV_2015
+           FROM INSURANCE
+           GROUP BY TIV_2015
+           HAVING COUNT(*) > 1),
+     B AS (SELECT LAT, LON
+           FROM INSURANCE
+           GROUP BY LAT, LON
+           HAVING COUNT(*) = 1)
+
 SELECT ROUND(SUM(TIV_2016), 2) TIV_2016
-FROM INSURANCE
-WHERE (LAT, LON) IN (SELECT LAT, LON
-                     FROM INSURANCE
-                     GROUP BY LAT, LON
-                     HAVING COUNT(*) = 1)
-  AND TIV_2015 IN (SELECT TIV_2015
-                   FROM INSURANCE
-                   GROUP BY TIV_2015
-                   HAVING COUNT(*) > 1)
+FROM INSURANCE C
+         JOIN A
+         JOIN B
+              ON C.TIV_2015 = A.TIV_2015 AND C.LAT = B.LAT AND C.LON = B.LON
